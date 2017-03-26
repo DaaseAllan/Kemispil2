cac3a78e15e31fac2f0ba8931901cd6f28d8189b
DaaseAllan <laurits.mumberg@gmail.com>
2017-03-26 19:03:43 +0200

    Scene 1 + array

---
+++ Kemiquiz/Assets/SpilManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SpilManager : MonoBehaviour {
+
+	public Question[] questions;
+	private static List<Question> unansweredQuestions;
+
+	private Question currentQuestion;
+
+	void start()
+	{
+		Debug.Log ("Startet");
+		if (unansweredQuestions == null || unansweredQuestions.Count == 0) {
+			unansweredQuestions = questions.ToList<Question> ();
+		}
+
+		getRandomQuestion ();
+		Debug.Log (currentQuestion.questionName+ " " + currentQuestion.svar);
+
+	}
+	void getRandomQuestion(){
+		int randomQuestionNummer = Random.Range (0, unansweredQuestions.Count);
+		currentQuestion = unansweredQuestions [randomQuestionNummer];
+
+		unansweredQuestions.RemoveAt (randomQuestionNummer);
+	}
+}
